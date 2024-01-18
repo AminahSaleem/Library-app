ea69e05677f8704096223380c29cd249431910cd
Aminah Saleem <aminah.saleem@mhr.co.uk>
2024-01-18 17:06:04 +0000

    created a service which allows for the books to be added to the local storage£

---
+++ library/src/app/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Book } from './library/book.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private storageKey = 'bookList';
+
+  constructor() {}
+
+  getBooks(): any[] {
+    const storedBooks = localStorage.getItem(this.storageKey);
+    //retrieves the stored books from local storage 
+    return storedBooks ? JSON.parse(storedBooks) : [];
+    //if there are stored books, parse the JSON string otherwise return an empty array
+  }
+
+  addBook(book: any): void {
+    // retrieves the exisiting books from local storage 
+    const books = this.getBooks();
+    books.push(book);
+    // add the new book to the array of exisiting books 
+    localStorage.setItem(this.storageKey, JSON.stringify(books));
+    //save the updated array of books baclk to local storage 
+  }
+}
--- library/src/app/library/library.component.ts
+++ library/src/app/library/library.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-library',
@@ -8,6 +9,8 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LibraryComponent {
 
+  constructor(private bookService: BookService) {}
+
   bookForm = new FormGroup({
     title: new FormControl(''),
     author: new FormControl(''),
@@ -15,8 +18,19 @@ export class LibraryComponent {
     description: new FormControl('')
   })
 
-  onSubmit() {
-    console.warn(this.bookForm.value)
-  }
+  //form group represents adding the form for adding books 
 
+  addBook(): void {
+    const newBook = { 
+      title: this.bookForm.value.title,
+      author: this.bookForm.value.author,
+      year: this.bookForm.value.year,
+      description: this.bookForm.value.description,
+    };
+    //new book created from the form values
+    this.bookService.addBook(newBook);
+    //passes it to the add book method of book service 
+    this.bookForm.reset();
+    //resets the form  
+  }
 }
